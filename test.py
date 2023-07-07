42ca24414ebfafee890038b6c258ced3afc550d7
echo <yuddecho@gmail.com>
2023-07-07 15:34:18 +0800

    finally

--- test.py
+++ test.py
@@ -4,11 +4,14 @@ finally_number = {}
 # seq_min, seq_max = 30, 622
 seq_min, seq_max = 20, 1186
 seq_count = 0
-root = '../../data'
+root = '../../data/db'
+
+test_acid_set = ['H', 'F', 'M', 'W', 'S', 'R', 'L', 'A', 'G', 'I', 'N', 'K', 'D', 'V', 'E', 'X', 'C', 'Q', 'T', 'P', 'Y']
+test_acid_set = set(test_acid_set)
 
 for fasta_file_name in files:
-    fasta_file = f'{root}/db/tmbed/{fasta_file_name}.fasta'
-    target_file = f'{root}/db/finally/{fasta_file_name}.fasta'
+    fasta_file = f'{root}/tmbed/{fasta_file_name}.fasta'
+    target_file = f'{root}/finally/{fasta_file_name}.fasta'
 
     with open(target_file, 'w', encoding='utf-8') as w:
         with open(fasta_file, 'r', encoding='utf-8') as r:
@@ -27,6 +30,15 @@ for fasta_file_name in files:
                 seq_len = len(seq)
                 if seq_len < seq_min or seq_len > seq_max:
                     continue
+
+                # 对训练集，限制氨基酸种类
+                if fasta_file_name == 'tt_pdb_solu' or fasta_file_name == 'tt_insolu':
+                    seq_set = set(seq)
+                    # 差集
+                    res_set = seq_set - test_acid_set
+                    if res_set:
+                        # 不为空
+                        continue
 
                 seq_count += 1
 
