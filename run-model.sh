ddb7e1e76fbad626057af79a8ba62784399667c3
echo <yuddecho@gmail.com>
2023-09-20 12:59:02 +0800

    kk

--- run-model.sh
+++ run-model.sh
@@ -1,4 +1,4 @@
-python dnabert-2-117m-two-classification.py --gpu 0
-python dnabert-2-117m-two-classification.py --gpu 1
-python dnabert-2-117m-two-classification.py --gpu 2
-python dnabert-2-117m-two-classification.py --gpu 3
+nohup python dnabert-2-117m-two-classification.py --gpu 0 > run0.log 2>&1 &
+nohup python dnabert-2-117m-two-classification.py --gpu 1 > run1.log 2>&1 &
+nohup python dnabert-2-117m-two-classification.py --gpu 2 > run2.log 2>&1 &
+nohup python dnabert-2-117m-two-classification.py --gpu 3 > run3.log 2>&1 &
